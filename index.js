cfce2a867932ada1ad758c7206146c82f2f231ef
linkel <ironcarbide@gmail.com>
2019-07-30 20:45:35 -0700

    Trying it out with table and tr td cells

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
-const expectedResult = document.querySelector("#expected-result");
-const actualResult = document.querySelector("#actual-result");
+const resultTable = document.querySelector("#result");
 const textInput = document.querySelector("#text-input");
 
 const formatText = () => {
+    resultTable.innerHTML = '';
+
     let myString = String(textInput.value);
 
     let expectedResult_startIndex = myString.search("to have been called") + 25;
@@ -11,21 +12,37 @@ const formatText = () => {
     let actualResult_startIndex = expectedResult_endIndex + 22;
     let actualResult_endIndex = myString.length - 1;
 
-    expectedResult.innerHTML = myString
-                                .substring(expectedResult_startIndex, expectedResult_endIndex)
-                                .split(',')
-                                .join(',\n');
+    let expectedResult = myString
+                        .substring(expectedResult_startIndex, expectedResult_endIndex)
+                        .split(/,/g)
 
-    actualResult.innerHTML = myString
-                                .substring(actualResult_startIndex, actualResult_endIndex)
-                                .split(',')
-                                .join(',\n');
+    let actualResult = myString
+                        .substring(actualResult_startIndex, actualResult_endIndex)
+                        .split(/,/g)
+                        
+    for (i = 0; i < expectedResult.length; i++) {
+        let row = document.createElement("tr")
+        let elineNum = document.createElement("td");
+        let elineData = document.createElement("td");
+        elineNum.className = "diff-line-number";
+        elineData.className = "diff-line";
+        elineNum.innerHTML = i+1;
+        elineData.innerHTML = expectedResult[i];
+
+        let alineNum = document.createElement("td");
+        let alineData = document.createElement("td");
+        alineNum.className = "diff-line-number";
+        alineData.className = "diff-line";
+        alineNum.innerHTML = i+1;
+        alineData.innerHTML = actualResult[i];
+
+        row.appendChild(elineNum);
+        row.appendChild(elineData);
+        row.appendChild(alineNum);
+        row.appendChild(alineData);
+        resultTable.appendChild(row);
+    }
 }
 
-const expectedDiff = () => {
-
-}
-
-const actualDiff = () => {
-    
-}
+textInput.innerHTML = "Expected spy EventServiceMock.getEventsObservable to have been called with [ Object({ eventGroupIds: [ 'ROOT' ], selfEventGroupIds: [ 'ROOT' ], startDate: Object({ year: 2019, month: 6, day: 17 }), endDate: Object({ year: 2019, month: 6, day: 24 }), manualOnly: false, trainableOnly: false, boutsOnly: false, pageNumber: 1, pageSize: 10, triggerIds: [  ], statusIds: [  ], behaviorIds: [  ], fighterIds: [  ], equipmentIds: [  ], includeAllFighterAndTrainerEvents: true, sortField: 'RecordDateLocal', sortDirection: 'desc' }) ] but actual calls were [ Object({ eventGroupIds: [ 'ROOT' ], selfEventGroupIds: [ 'ROOT' ], startDate: Object({ year: 2019, month: 6, day: 17 }), endDate: Object({ year: 2019, month: 6, day: 24 }), manualOnly: false, trainableOnly: false, boutsOnly: false, pageNumber: 1, pageSize: 10, triggerIds: [  ], statusIds: [  ], behaviorIds: [  ], fighterIds: [  ], equipmentIds: [  ], cameraIds: [  ], includeAllFighterAndTrainerEvents: true, sortField: 'RecordDateLocal', sortDirection: 'desc' }) ]."
+formatText();
